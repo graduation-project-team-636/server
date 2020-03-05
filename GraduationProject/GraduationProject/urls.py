c2fc9c010bb2e87d49278007bcdfdff3c56fa00d
HisBread <610260811@qq.com>
2020-03-05 16:19:56 +0800

    add stream_video method in Video

--- GraduationProject/GraduationProject/urls.py
+++ GraduationProject/GraduationProject/urls.py
@@ -17,12 +17,14 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from django.conf import settings
 from django.conf.urls.static import static
+import video
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/course/', include('course.urls')),
     url(r'^api/video/', include('video.urls')),
     url(r'^api/', include('login.urls')),
+    url(r'^media/video/', video.views.stream_video),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.PPT_URL, document_root=settings.PPT_ROOT)
--- GraduationProject/video/views.py
+++ GraduationProject/video/views.py
@@ -5,11 +5,14 @@ import course
 import json
 import time
 import string
+import mimetypes
 import static.tools.global_settings as global_settings
 from navigation.video import VidToSlides
 import os
 import threading
-import _thread
+import re
+from wsgiref.util import FileWrapper
+from django.http import StreamingHttpResponse
 
 # Create your views here.
 
@@ -273,3 +276,49 @@ def getppt(request):
         # 返回参数
         response['data']['ppt'] = tmp_ppts
         return JsonResponse(response)
+
+
+def file_iterator(file_name, chunk_size=8192, offset=0, length=None):
+    with open(file_name, "rb") as f:
+        f.seek(offset, os.SEEK_SET)
+        remaining = length
+        while True:
+            bytes_length = chunk_size if remaining is None else min(
+                remaining, chunk_size)
+            data = f.read(bytes_length)
+            if not data:
+                break
+            if remaining:
+                remaining -= len(data)
+            yield data
+
+
+def stream_video(request):
+    """将视频文件以流媒体的方式响应"""
+    path = os.getcwd() + request.path.replace('/', '\\')
+    print(path)
+    range_header = request.META.get('HTTP_RANGE', '').strip()
+    range_re = re.compile(r'bytes\s*=\s*(\d+)\s*-\s*(\d*)', re.I)
+    range_match = range_re.match(range_header)
+    size = os.path.getsize(path)
+    content_type, encoding = mimetypes.guess_type(path)
+    content_type = content_type or 'application/octet-stream'
+    if range_match:
+        first_byte, last_byte = range_match.groups()
+        first_byte = int(first_byte) if first_byte else 0
+        last_byte = first_byte + 1024 * 1024 * 8    # 8M 每片,响应体最大体积
+        if last_byte >= size:
+            last_byte = size - 1
+        length = last_byte - first_byte + 1
+        resp = StreamingHttpResponse(file_iterator(
+            path, offset=first_byte, length=length), status=206, content_type=content_type)
+        resp['Content-Length'] = str(length)
+        resp['Content-Range'] = 'bytes %s-%s/%s' % (
+            first_byte, last_byte, size)
+    else:
+        # 不是以视频流方式的获取时，以生成器方式返回整个文件，节省内存
+        resp = StreamingHttpResponse(FileWrapper(
+            open(path, 'rb')), content_type=content_type)
+        resp['Content-Length'] = str(size)
+    resp['Accept-Ranges'] = 'bytes'
+    return resp
